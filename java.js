79ae0aadb6735bd4f6eb6cccdf60eef58510fa79
SurajS2000 <suryasuraj437@gmail.com>
2023-10-14 16:42:55 +0530

    done

--- java.js
+++ java.js
@@ -1,31 +1,15 @@
 const maincontainer = document.querySelector(".container");
 const thank = document.querySelector(".container2")
-const submitButton=document.getElementsByClassName("submit");
+const submitButton=document.getElementById("submit");
 const rates=document.querySelectorAll(".btn");
 const rating=document.getElementById("rating");
 let rate=5;
 
-function thankyou() {
+submitButton.addEventListener("click",function(){
     thank.classList.remove("hidden");
     maincontainer.style.display="none";
-}
-function one(){
-    rate=1;
-    rating.innerHTML=rate
-}
-function two(){
-    rate=2;
-    rating.innerHTML=rate
-}
-function three(){
-    rate=3;
-    rating.innerHTML=rate
-}
-function four(){
-    rate=4;
-    rating.innerHTML=rate
-}
-function five(){
-    rate=5;
-    rating.innerHTML=rate
+    rating.innerHTML=rate;
+})
+for(var i=0;i<rates.length;i++){
+    rates[i].addEventListener("click",function(){rate=this.innerHTML})
 }
